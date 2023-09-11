00c62e22d47cfd8956d86b9e83015cfa8cfdaa8d
lorians22 <lorians@purdue.edu>
2023-09-11 00:20:14 -0400

    cleaned things up

--- individual_calibration.py
+++ individual_calibration.py
@@ -1,16 +1,24 @@
 #Author: Logan Orians
 #Date: 08/19/23
 
+### USAGE
+# individual_calibration.py <path_to_images> <filetype>
+# ex:  individual_calibration.py images/individual/NB2B jpg
+
+#NOTES:
+# NB2B seems to use 12/18 images for calibration
+# NB3A seems to use 15/17 images for calibration
+# goal was 10, so should be fine
+# not sure why NB2B is so much worse than NB3A...reflections?
+
 import sys
 import cv2
 import numpy as np
 import glob
+import pathlib
 
-def main():
-
-	filepath = str(sys.argv[1]) #path to images you want to use for calibration
-	filetype = '.jpg' #filetype of images you want to use for calibration
-
+# Perform camera calibration
+def indiv_calibration(filepath, filetype):
 	#termination criteria for subpixel precision calculation
 	criteria = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001
 
@@ -37,31 +45,46 @@ def main():
 			corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria) #increase subpixel precision
 			imagePoints.append(corners2)
 
+			# Display corners (can comment out)
+			#cv2.drawChessboardCorners(image, chSize, corners2, ret)
+			#cv2.imshow('img', image)
+			#cv2.waitKey(1000) #display image for 1 second
+
 	# Camera calibration
 	ret, camMatrix, distCoeffs, rotVecs, transVecs = cv2.calibrateCamera(objectPoints, imagePoints, gray.shape[::-1], None, None)
 
-	# Print results
+	# Print results (can comment out)
 	print('Camera Matrix:\n', camMatrix)
 	print('Distortion Coefficients', distCoeffs)
 
-	# Setup output file
-	cameraModel = filepath.rsplit('/', 1)[-1] #determine output file name
-	outFile = cameraModel+'.txt' #set output file name
-	FILE = open(outFile, 'w') #clear file
-	FILE = open(outFile, 'a') #open file for appending
+	return camMatrix, distCoeffs, rotVecs, transVecs
 
-	# Write results to output file
-	camMatrix.tofile(FILE, sep=' ') #write camera matrix to file
-	FILE.write('\n') #separate by newline
-	distCoeffs.tofile(FILE, sep=' ') #write distortion coefficients to file
-	FILE.close() #close file
-
-	# Sanity check: make sure things look okay (straight lines) by undistorting image
-	image = cv2.imread(filepath+'/image00.jpg') #hardcode straight-on image
+# Visually check if calibrated chBoard is "flat" and has straight lines
+def sanity_check(filepath, filetype, camMatrix, distCoeffs):
+	image = cv2.imread(filepath+'/image00'+filetype) #read in test image
 	undistImage = cv2.undistort(image, camMatrix, distCoeffs, None, camMatrix) #undistort image
 	cv2.imshow('Undistorted Image', undistImage) #show image
 	cv2.waitKey(0) #wait for keypress
 	cv2.destroyAllWindows() #close image viewer
 
+	return
+
+# Write camera matrix and distortion coefficients if satisfied with sanity check
+def write_results(filepath, camMatrix, distCoeffs):
+	# Setup output file
+	path = pathlib.PurePath(filepath)
+	outFile = path.name+'.txt' #set output file name
+	FILE = open(outFile, 'w') #clear file
+	FILE = open(outFile, 'a') #open file for appending
+
+	camMatrix.tofile(FILE, sep=' ') #write camera matrix to file
+	FILE.write('\n') #separate by newline for importing later with readline()
+	distCoeffs.tofile(FILE, sep=' ') #write distortion coefficients to file
+	FILE.close() #close file
+
 if __name__ == '__main__':
-	main()
+	filepath = sys.argv[1] #path to images you want to use for calibration
+	filetype = '.'+sys.argv[2] #filetype of images you want to use for calibration
+	camMatrix, distCoeffs, rotVecs, transVecs = indiv_calibration(filepath, filetype)
+	sanity_check(filepath, filetype, camMatrix, distCoeffs)
+	write_results(filepath, camMatrix, distCoeffs)
