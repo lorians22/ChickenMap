4bf382a1c3ed603463a3ad3a47a98993d2f390bd
lorians22 <lorians@purdue.edu>
2023-11-13 16:58:59 -0500

    update install command files

--- REQS_WIN.cmd
+++ REQS_WIN.cmd
@@ -1,6 +1,5 @@
 @echo off
-pip3 install -r .requirements.txt
 attrib +h .options.json
 attrib +h .requirements.txt
-echo: && echo: && echo: && echo INSTALLATION COMPLETE. YOU MAY NOW CLOSE THIS WINDOW
+pip3 install -r .requirements.txt && echo: && echo: && echo: && echo INSTALLATION COMPLETE. YOU MAY NOW CLOSE THIS WINDOW
 pause
